9eb0c359c69cb97218fd067b22c5bcca7ec6dcbb
thakursachin1004 <sachinst000@gmail.com>
2022-10-01 17:54:58 +0530

    feature/react routing setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import StockList from "./pages/list";
+import Quotes from "./pages/quotes";
+import { Redirect } from "react-router";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={StockList} />
+          <Route path="/stocks/quotes/:symbol" exact component={Quotes} />
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
